f5448d071e9d9e2c419315a28032916f4eeda5cd
Victor <nhacuteo001@.com>
2025-09-24 16:50:22 +0800

    Created 2 files

---
+++ Victor_Minus.py
@@ -0,0 +1,14 @@
+#
+# Victor, 2025/09/24
+# File: Victor_minus.py
+# Short description of the task
+#
+
+# 1. Input
+X = 8
+Y = 7
+
+# 2. Process
+Sum = X - Y
+# 3. Output
+print (f'Result {Sum}')
---
+++ Victor_Sum.py
@@ -0,0 +1,14 @@
+#
+# Victor, 2025/09/24
+# File: Victor_Sum.py
+# Add two numbers then print
+#
+
+# 1. Input
+X = 8
+Y = 7
+
+# 2. Process
+Sum = X + Y
+# 3. Output
+print (f'Result {Sum}')
